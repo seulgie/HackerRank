1a0413119d2e2fc4b5916ce9025d6fba2b6e1abb
Seulgie <gkstmfrl917@naver.com>
2024-04-05 11:46:50 +0200

    Create collections.Counter().py

---
+++ Python/Collections/collections.Counter().py
@@ -0,0 +1,17 @@
+# Link : https://www.hackerrank.com/challenges/collections-counter/problem
+# Difficulty : Easy
+
+import collections
+
+NUMBER_OF_SHOES = int(input())
+SIZES_IN_STOCK = collections.Counter(map(int, input().split()))
+
+TOTAL_REVENUE = 0
+
+for _ in range(int(input())):
+  size, price = map(int, input().split())
+  if SIZES_IN_STOCK[size]:
+      TOTAL_REVENUE  += price
+      SIZES_IN_STOCK[size] -= 1
+      
+print(TOTAL_REVENUE)
