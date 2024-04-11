76aca74473aeede3f3f9c0a9d8933c540f87f458
Seulgie <gkstmfrl917@naver.com>
2024-04-11 15:58:35 +0900

    Create IntegersComeInAllSizes.py

---
+++ Python/Math/IntegersComeInAllSizes.py
@@ -0,0 +1,13 @@
+"""
+Title     : Integers Come In All Sizes
+Subdomain : Math
+Domain    : Python
+Problem   : https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+"""
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(pow(a,b) + pow(c,d))
