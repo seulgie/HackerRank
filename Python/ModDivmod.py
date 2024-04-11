26f579ce8e972a435639aaf02b622ff590414bd3
Seulgie <gkstmfrl917@naver.com>
2024-04-11 10:38:01 +0900

    Create ModDivmod.py

---
+++ Python/ModDivmod.py
@@ -0,0 +1,13 @@
+"""
+Title     : Mod Divmod
+Subdomain : Math
+Domain    : Python
+Problem   : https://www.hackerrank.com/challenges/python-mod-divmod/problem
+"""
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+
+print(a//b)
+print(a%b)
+print(divmod(a,b))
