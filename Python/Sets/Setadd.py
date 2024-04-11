b0e883f151b115f7f57f5d57e709d2400828ff71
Seulgie <gkstmfrl917@naver.com>
2024-04-11 15:34:27 +0900

    Create Setadd.py

---
+++ Python/Sets/Setadd.py
@@ -0,0 +1,15 @@
+"""
+Title     : Set .add()
+Subdomain : Sets
+Domain    : Python
+problem   : https://www.hackerrank.com/challenges/py-set-add/problem
+"""
+
+n = int(input())
+country_set = set()
+
+for _ in range(n):
+  country_set.add(input())
+
+print(len(set))
+
