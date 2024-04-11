daeb06441d41e8a126d6ac25633a346866ffbcfe
Seulgie <gkstmfrl917@naver.com>
2024-04-11 15:56:14 +0900

    Create PowerModPower.py

---
+++ Python/Math/PowerModPower.py
@@ -0,0 +1,12 @@
+"""
+Title     : Power - Mod Power
+Subdomain : Math
+Domain    : Python
+Problem   : https://www.hackerrank.com/challenges/python-power-mod-power/problem
+"""
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+m = int(input())
+print(pow(a, b))
+print(pow(a, b, m))
