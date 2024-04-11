3c92b123d81e07e58d81c246917df8f153164800
Seulgie <gkstmfrl917@naver.com>
2024-04-11 14:05:01 +0900

    Create FindingthePercentage.py

---
+++ Python/Basic Data Types/FindingthePercentage.py
@@ -0,0 +1,17 @@
+"""
+Title     : Finding the percentage
+Subdomain : Data Types
+Domain    : Python
+Problem   : https://www.hackerrank.com/challenges/finding-the-percentage/problem
+"""
+
+if __name__ == "__main__":
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    ans = sum(student_marks[query_name]) / len(student_marks[query_name])
+    print(f"{ans:.2f}")
