0bb0e0eb01f3faf9a4286d24becd704566e7494a
Seulgie <gkstmfrl917@naver.com>
2024-04-11 13:47:26 +0900

    Create WhatsYourName.py

---
+++ Python/Strings/WhatsYourName.py
@@ -0,0 +1,14 @@
+"""
+Title     : Whats Your Name?
+Subdomain : Strings
+Domain    : Python
+Problem   : https://www.hackerrank.com/challenges/whats-your-name/problem
+"""
+
+def print_full_name(first, last):    
+    print(f"Hello {first} {last}! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
